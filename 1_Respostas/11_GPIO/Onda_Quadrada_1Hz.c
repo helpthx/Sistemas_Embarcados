5a2f85d42ef0e0d47304edbcab17403f417a8f61
João Vitor Rodrigues Baptista <jvrbaptista@live.com>
2018-11-18 16:37:10 -0200

    comentando o codigo

--- 1_Respostas/11_GPIO/Onda_Quadrada_1Hz.c
+++ 1_Respostas/11_GPIO/Onda_Quadrada_1Hz.c
@@ -1,3 +1,5 @@
+/*Escreva um código em C para gerar uma onda quadrada de 1 Hz em um pino GPIO do Raspberry Pi*/
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -8,7 +10,7 @@ int fd;
 
 void fechar(){
 	close(fd);
-	fd = open("/sys/class/gpio/unexport",O_WRONLY);
+	fd = open("/sys/class/gpio/unexport",O_WRONLY); //unexportanto o GPIO para n ter problemas.
 	write(fd,"16",2);
 	printf("Fechando programa\n");
 	close(fd);
@@ -17,35 +19,34 @@ void fechar(){
 }
 
 
-
-
 int main(){
-
+	//Sinal para fechar corretamente o programa.
 	signal(SIGINT,fechar);
 
 	//Setando como export
 	printf("Realizando o export\n");
 	fd = open("/sys/class/gpio/export",O_WRONLY);
-	write(fd,"16",2);
+	write(fd,"4",2);
 	close(fd);
 	sleep(1);
 
 	//Setando como saída
 	printf("Iniciando o pin como saída\n");
-	fd = open("/sys/class/gpio/gpio16/direction",O_WRONLY);
+	fd = open("/sys/class/gpio/gpio4/direction",O_WRONLY);
 	write(fd,"out",4);
 	close(fd);
 	sleep(1);
 
 
 
-	fd = open("/sys/class/gpio/gpio16/value",O_WRONLY);
+	fd = open("/sys/class/gpio/gpio4/value",O_WRONLY);
 	printf("Iniciando o blink\n");
+	/*Função que ira ficar sobescrevendo o gpio 4 acada 1/1s = 1hz */
 	while(1){
 		write(fd,"1",2);
-		usleep(500000);
+		usleep(500000); // 0.5s 
 		write(fd,"0",2);
-		usleep(500000);
+		usleep(500000); // 0.5s
 	}
 
 	return 0;
